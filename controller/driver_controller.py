d4f15c5e7a3b02c430cf49f83d56b2513e83166c
bowo X daily <ikhwanudinaldi04@gmail.com>
2022-06-02 21:27:36 +0700

    Depen

--- controller/driver_controller.py
+++ controller/driver_controller.py
@@ -3,9 +3,9 @@ from Interface.driver_operation import DriverOperation
 class DriverController(DriverOperation):
 
 
-    def menolak_pesanan(self):
-        print("Driver menolak pesanan karena lokasi tidak terjangkau")
+    def menolak_pesanan(self)-> None:
+            print("Driver menolak pesanan karena lokasi tidak terjangkau")
 
 
-    def mengantarkan_pesanan (self):
-        print("Driver mengantarkan Pesanan dari Penjual Ke Pembeli") 
+    def mengantarkan_pesanan (self)-> None:
+            print("Driver mengantarkan Pesanan dari Penjual Ke Pembeli") 
--- controller/penjual_controller.py
+++ controller/penjual_controller.py
@@ -2,9 +2,9 @@ from Interface.penjual_operation import PenjualOperation
 
 class PenjualController(PenjualOperation):
 
-    def menolak_pesanan(self):
+    def menolak_pesanan(self)-> None:
         print ("Penjual Menolak Pesanan, Stok Habis")
 
 
-    def menyiapkan_pesanan(self):
+    def menyiapkan_pesanan(self)-> None:
         print ("Penjual Menyiapkan pesanan Seusai Pilihan")    
